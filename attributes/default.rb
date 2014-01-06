952709062d96cce3fc8c641fe5d277c550c63513
Michael Stucki <michael.stucki@typo3.org>
2014-01-06 11:12:08 +0100

    Delete unused locales

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,6 @@
 default["locales"]["generate"] = [
-  "en_US ISO-8859-1",
-  "en_US.ISO-8859-15 ISO-8859-15",
   "en_US.UTF-8 UTF-8",
-  "de_DE ISO-8859-1",
-  "de_DE.UTF-8 UTF-8",
-  "de_DE@euro ISO-8859-15"
+  "de_DE.UTF-8 UTF-8"
 ]
 
 default["locales"]["default"] = "en_US.UTF-8"
