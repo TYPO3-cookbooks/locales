21326c3a1a8eece0d8bcad6ff8763f5356988f09
Michael Stucki <michael.stucki@typo3.org>
2014-01-16 14:40:43 +0100

    Raise version to 1.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache2"
 description      "Installs/Configures locales"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 recipe           "locales", "Sets up locales on the server and sets a server time zone."
 
 
